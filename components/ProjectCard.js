fe540b35b169b0f3aa435dd1abdf644455675a2a
Rhoda P <rhodaphillipsosei@gmail.com>
2022-05-24 20:08:13 -0400

    dynamic render of projects pg, styling

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -9,18 +9,19 @@ import styles from '../styles/Projects.module.css'
 
 const data = [
   {
-    image: "/tweeterdemo.gif",
+    image: "/tweeter-thumbnail.png",
     title: "Tweeter",
     description:
-      "A single-paged clone of a certain popular social media site ;-)",
+      "A clone of a particular social media site ;-)",
     technologies: "Javascript, jQuery, AJAX, HTML, CSS",
     github: "https://github.com/rphillipsosei/tweeter",
     height: "400px",
     width: "750px",
     id: "tweeter",
+    demo: "SEE DEMO",
   },
   {
-    image: "/moonbeamdemo.gif",
+    image: "/moonbeam-thumbnail.png",
     title: "Moonbeam Trading Company",
     description: "A company website to be deployed soon",
     technologies: "React, CSS, Material UI, Theme UI",
@@ -28,28 +29,31 @@ const data = [
     height: "400px",
     width: "220px",
     id: "moonbeam",
-    
+    demo: "SEE DEMO",
+
   },
   {
-    image: "/tinyappdemo.gif",
+    image: "/tinyapp-thumbnail.png",
     title: "TinyApp",
     description:
-      "An app where users can create, edit, and store shortened links for everyday websites.",
+      "A URL shortener for everyday websites.",
     technologies: "Express, Node.js, bCrypt, EJS, Bootstrap",
     github: "https://github.com/rphillipsosei/tinyapp",
     height: "400px",
     width: "750px",
     id: "tinyapp",
+    demo: "SEE DEMO",
   },
   {
-    image: "/portfolio.png",
+    image: "/portfolio-thumbnail.png",
     title: "Profile App",
-    description: "This very app. For you to get to know more about me!",
-    technologies: "React, NextJS, Bootstrap, Framer Motion",
+    description: " For you to get to know more about me!",
+    technologies: "React, NextJS, Material UI, Framer Motion",
     github: "https://github.com/rphillipsosei/new-portfolio-app",
     height: "400px",
     width: "750px",
     id: "portfolio",
+ 
   },
 ];
 
@@ -57,12 +61,16 @@ export default function ProjectCard() {
 
   return (
     <div className={styles.projectsbox}>
+     <h1>PROJECTS</h1>
+     <div className={styles.cardrow}>
    {data.map((project) => {
-<Card className={styles.cardcontainer} sx={{ maxWidth: 345, maxHeight: 450 }}>
-          <Typography align="center" gutterBottom variant="h5" component="div">
+     return (
+<Card className={styles.cardcontainer} sx={{ maxWidth: 300, maxHeight: 450 }}>
+          <Typography align="center" gutterBottom variant="h5" component="div" className={styles.projecttitle}>
           {project.title}
         </Typography>
         <CardMedia
+        className={styles.democontainer}
         component="img"
         alt="green iguana"
         height="140"
@@ -70,14 +78,18 @@ export default function ProjectCard() {
       />
       <CardContent>
     
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2" color="text.secondary" className={styles.projectdesc}>
           {project.description}
+          <br></br>
+          <br></br>
+          STACK<br></br>
+          {project.technologies}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">VIEW SOURCE CODE</Button>
-        <Button size="small">SEE DEMO</Button>
+      <CardActions className={styles.projectbuttons}>
+        <Button className={styles.projectbtn} size="small"><b>VIEW SOURCE CODE</b></Button>
+        <Button className={styles.projectbtn} id={project.demo} size="small"><b>{project.demo}</b></Button>
       </CardActions>
-    </Card>
-    })}</div>
+    </Card>)
+    })}</div></div>
   )}
--- pages/projects.js
+++ pages/projects.js
@@ -1,13 +1,5 @@
 import Nav from "../components/Nav.js";
 import styles from "../styles/Projects.module.css";
-import { Grid } from "@mui/material";
-import Image from "next/image";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import ProjectCard from "../components/ProjectCard"
 
 
@@ -18,57 +10,10 @@ export default function Projects() {
       <div className={styles.projectscontents}>
        
          
-{<ProjectCard/>}
+<ProjectCard/>
 
        
       </div>
     </div>
   );
 }
-
-const projectData = [
-  {
-    image: "/tweeterdemo.gif",
-    title: "Tweeter",
-    description:
-      "A single-paged clone of a certain popular social media site ;-)",
-    technologies: "Javascript, jQuery, AJAX, HTML, CSS",
-    github: "https://github.com/rphillipsosei/tweeter",
-    height: "400px",
-    width: "750px",
-    id: "0",
-  },
-  {
-    image: "/moonbeamdemo.gif",
-    title: "Moonbeam Trading Company",
-    description: "A company website to be deployed soon",
-    technologies: "React, CSS, Material UI, Theme UI",
-    github: "https://github.com/rphillipsosei/moonbeam-tc/",
-    height: "400px",
-    width: "220px",
-    id: "1",
-    
-  },
-  {
-    image: "/tinyappdemo.gif",
-    title: "TinyApp",
-    description:
-      "An app where users can create, edit, and store shortened links for everyday websites.",
-    technologies: "Express, Node.js, bCrypt, EJS, Bootstrap",
-    github: "https://github.com/rphillipsosei/tinyapp",
-    height: "400px",
-    width: "750px",
-    id: "2",
-  },
-  {
-    image: "/portfolio.png",
-    title: "Profile App",
-    description: "This very app. For you to get to know more about me!",
-    technologies: "React, NextJS, Bootstrap, Framer Motion",
-    github: "https://github.com/rphillipsosei/new-portfolio-app",
-    height: "400px",
-    width: "750px",
-    id: "3",
-  },
-];
-
